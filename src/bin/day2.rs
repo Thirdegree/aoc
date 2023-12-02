f9f4d8d6e7ea13a6f3f81faf0e6bf464ed7f3322
Thirdegree <noreply@no.com>
2023-12-02 10:44:27 +0100

    impl FromStr for Game; also add some tests

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 // ID was used in part 1 but not part 2, but still part of the "Game" struct, but I don't want
 // cargo yelling at me.
 #[allow(dead_code)]
+#[derive(PartialEq, Debug)]
 struct Game {
     id: u32,
     max_blue: u32,
@@ -8,35 +11,68 @@ struct Game {
     max_green: u32,
 }
 
-fn parse_line(line: &str) -> Game {
-    let (id_str, grabs) = line.split_once(':').unwrap();
-    let id_num = id_str.strip_prefix("Game ").unwrap();
-    let mut game = Game {
-        id: id_num.parse().unwrap(),
-        max_red: 0,
-        max_blue: 0,
-        max_green: 0,
-    };
-    for grab in grabs.split(';') {
-        for marble in grab.split(',') {
-            let marble = marble.trim();
-            let (count, color) = marble.split_once(' ').unwrap();
-            match color {
-                "blue" => game.max_blue = game.max_blue.max(count.parse().unwrap()),
-                "green" => game.max_green = game.max_green.max(count.parse().unwrap()),
-                "red" => game.max_red = game.max_red.max(count.parse().unwrap()),
-                _ => unreachable!(),
+impl FromStr for Game {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (id_str, grabs) = s.split_once(':').unwrap();
+        let id_num = id_str.strip_prefix("Game ").unwrap();
+        let mut game = Game {
+            id: id_num.parse()?,
+            max_red: 0,
+            max_blue: 0,
+            max_green: 0,
+        };
+        for grab in grabs.split(';') {
+            for marble in grab.split(',') {
+                let marble = marble.trim();
+                let (count, color) = marble.split_once(' ').unwrap();
+                match color {
+                    "blue" => game.max_blue = game.max_blue.max(count.parse()?),
+                    "green" => game.max_green = game.max_green.max(count.parse()?),
+                    "red" => game.max_red = game.max_red.max(count.parse()?),
+                    _ => unreachable!(),
+                }
             }
         }
+        Ok(game)
     }
-    game
 }
 
 fn main() {
     let result: u32 = include_str!("../day2/input.txt")
         .lines()
-        .map(parse_line)
-        .map(|game| game.max_green * game.max_blue * game.max_red)
+        .filter_map(|line| line.parse().ok())
+        .map(|game: Game| game.max_green * game.max_blue * game.max_red)
         .sum();
     println!("Day 2 result: {result}")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(
+            Ok(Game {
+                id: 5,
+                max_red: 1,
+                max_blue: 2,
+                max_green: 3
+            }),
+            "Game 5: 1 red; 2 blue, 3 green".parse()
+        );
+    }
+    #[test]
+    fn test_parse_game_with_missing_counts() {
+        assert_eq!(
+            Ok(Game {
+                id: 11,
+                max_red: 0,
+                max_blue: 0,
+                max_green: 200
+            }),
+            "Game 11: 200 green".parse()
+        )
+    }
 }
