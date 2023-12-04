af4c9f162907149fee75faba1c7e882efecac344
Thirdegree <noreply@no.com>
2023-12-04 10:24:39 +0100

    day 3 & 4

---
+++ src/bin/day3.rs
@@ -0,0 +1,126 @@
+use std::collections::HashMap;
+
+struct Schematic {
+    board: Vec<Vec<char>>,
+}
+
+#[derive(Debug)]
+struct Part {
+    row: usize,
+    span: (usize, usize), // inclusive
+    symbols: Vec<Symbol>,
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Symbol {
+    symbol: char,
+    location: (usize, usize), // row, col
+}
+
+impl From<&str> for Schematic {
+    fn from(value: &str) -> Self {
+        Schematic {
+            board: value.lines().map(|l| l.chars().collect()).collect(),
+        }
+    }
+}
+
+impl Schematic {
+    fn adjacent_symbols(&self, part: &Part) -> Vec<Symbol> {
+        let min_row = if part.row > 0 { part.row - 1 } else { 0 };
+        let min_col = if part.span.0 > 0 { part.span.0 - 1 } else { 0 };
+        let max_row = (part.row + 1).min(self.board.len() - 1);
+        let max_col = (part.span.1 + 1).min(self.board[0].len() - 1);
+        let mut symbols = vec![];
+        for row in min_row..=max_row {
+            for col in min_col..=max_col {
+                if !(self.board[row][col] == '.' || self.board[row][col].is_ascii_digit()) {
+                    symbols.push(Symbol {
+                        symbol: self.board[row][col],
+                        location: (row, col),
+                    })
+                }
+            }
+        }
+        symbols
+    }
+    fn find_parts(&self) -> Vec<Part> {
+        self.board
+            .iter()
+            .enumerate()
+            .flat_map(|(idx, line)| {
+                let mut parts = vec![];
+                let mut cur_part_start: Option<usize> = None;
+                for (cidx, char) in line.iter().enumerate() {
+                    if (!char.is_ascii_digit() || cidx == self.board[0].len() - 1)
+                        && cur_part_start.is_some()
+                    {
+                        // Just hit the END of a part span (either no longer digit, or end of a line)
+                        let mut part = Part {
+                            row: idx,
+                            span: (cur_part_start.unwrap(), cidx - 1),
+                            symbols: vec![],
+                        };
+                        let symbols = self.adjacent_symbols(&part);
+                        part.symbols = symbols;
+                        if !part.symbols.is_empty() {
+                            parts.push(part);
+                        }
+                        cur_part_start = None
+                    } else if char.is_ascii_digit() && cur_part_start.is_none() {
+                        // Just hit the START of a part span
+                        cur_part_start = Some(cidx);
+                    }
+                }
+                parts
+            })
+            .collect()
+    }
+    fn part_number(&self, part: &Part) -> u32 {
+        let row = &self.board[part.row];
+        row[part.span.0..=part.span.1]
+            .iter()
+            .collect::<String>()
+            .parse()
+            .unwrap()
+    }
+    fn gear_pairs<'a>(&'a self, parts: &'a [Part]) -> Vec<(&Part, &Part)> {
+        let mut shared_symbols = HashMap::new();
+        for (symbol, part) in parts
+            .iter()
+            .flat_map(|p| p.symbols.iter().map(move |s| (s, p)))
+        {
+            if symbol.symbol != '*' {
+                continue;
+            }
+            shared_symbols
+                .entry(symbol)
+                .or_insert_with(Vec::new)
+                .push(part);
+        }
+        shared_symbols
+            .values()
+            .filter_map(|parts| {
+                if parts.len() == 2 {
+                    Some((parts[0], parts[1]))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+}
+
+fn main() {
+    let board: Schematic = include_str!("../day3/input.txt").into();
+    let parts = board.find_parts();
+    let gear_pairs = board.gear_pairs(&parts);
+    println!(
+        "{:?}",
+        gear_pairs
+            .iter()
+            .map(|(g1, g2)| { board.part_number(g1) * board.part_number(g2) })
+            .sum::<u32>()
+    );
+    // println!("Result for day 3: {}", parts.iter().sum::<u32>());
+}
---
+++ src/bin/day4.rs
@@ -0,0 +1,63 @@
+use std::collections::HashSet;
+
+#[allow(dead_code)]
+struct Card {
+    id: u32,
+    winning_numbers: HashSet<u32>,
+    card_numbers: HashSet<u32>,
+}
+
+impl From<&str> for Card {
+    fn from(value: &str) -> Self {
+        let (id_str, numbers) = value.split_once(": ").unwrap();
+        let id = id_str.strip_prefix("Card ").unwrap();
+        let (winning_numbers, card_numbers) = numbers.split_once('|').unwrap();
+        Card {
+            id: id.trim().parse().unwrap(),
+            winning_numbers: winning_numbers
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect(),
+            card_numbers: card_numbers
+                .split_whitespace()
+                .map(|n| n.trim().parse().unwrap())
+                .collect(),
+        }
+    }
+}
+
+impl Card {
+    fn winning_numbers(&self) -> HashSet<&u32> {
+        self.winning_numbers
+            .intersection(&self.card_numbers)
+            .collect()
+    }
+}
+
+fn main() {
+    let cards: Vec<Card> = include_str!("../day4/input.txt")
+        .lines()
+        .map(|line| line.into())
+        .collect();
+    let mut count_cards_remain = vec![1; cards.len()];
+    let mut tot_cards = 0;
+    let mut still_going = true;
+    while still_going {
+        still_going = false;
+        let to_inc: Vec<_> = count_cards_remain
+            .iter_mut()
+            .enumerate()
+            .filter(|(_, &mut c)| c != 0)
+            .flat_map(|(idx, c)| {
+                *c -= 1;
+                tot_cards += 1;
+                idx + 1..=cards[idx].winning_numbers().len() + idx
+            })
+            .collect();
+        to_inc.iter().for_each(|&c| count_cards_remain[c] += 1);
+        if !to_inc.is_empty() {
+            still_going = true;
+        }
+    }
+    println!("Day 4 result: {tot_cards}");
+}
